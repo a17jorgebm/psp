bfe12f1dce54338220bd475b33fa5bbf69ef3b35
dawMD - Blanco Mariño <A17JorgeBM@sanclemente.local>
2024-10-15 12:26:53 +0200

    contador vocales

--- RuntimeExec/src/main/java/org/example/Ejer10ContarVocales/ContarVocales.java
+++ RuntimeExec/src/main/java/org/example/Ejer10ContarVocales/ContarVocales.java
@@ -1,5 +1,6 @@
 package org.example.Ejer10ContarVocales;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,12 +10,12 @@ import java.util.regex.Pattern;
 public class ContarVocales {
     public static void main(String[] args) {
         if (args.length<2){
-            System.err.println("Arumentos inválidos, uso: ContarVocales nomeFicheiro letra");
+            System.err.println("Argumentos inválidos, uso: ContarVocales nomeFicheiro letra");
             System.exit(1);
         }
 
         Path file= Paths.get(args[0]);
-        if (Files.exists(file)){
+        if (!Files.exists(file)){
             System.err.println("O ficheiro "+ file.getFileName() +" non existe");
             System.exit(2);
         }
@@ -22,11 +23,26 @@ public class ContarVocales {
         String letra=args[1];
         int contador=0;
 
-        Matcher matcher= Pattern.compile("a",Pattern.CASE_INSENSITIVE).matcher(letra);
-        while (matcher.find()){
-            contador++;
+        Pattern patron= Pattern.compile(letra,Pattern.CASE_INSENSITIVE);
+
+        try(BufferedReader input=new BufferedReader(new FileReader(file.toFile()))){
+            String linea;
+            while ((linea=input.readLine())!=null){
+                Matcher matcher=patron.matcher(linea);
+                while (matcher.find()){
+                    contador++;
+                }
+            }
+        }catch (IOException e){
+            System.err.println("Error ao ler o ficheiro: "+e.getMessage());
+            System.exit(2);
         }
 
-        System.out.print(contador);
+        try(BufferedWriter output=new BufferedWriter(new FileWriter("ficheiroSaida.txt",true))){
+            output.write("Letra a: "+contador+"\n");
+        }catch (IOException e){
+            System.err.println("Error ao escribir o ficheiro: "+e.getMessage());
+            System.exit(3);
+        }
     }
 }
--- RuntimeExec/src/main/java/org/example/Ejer10ContarVocales/Ejer10ContarVocales.java
+++ RuntimeExec/src/main/java/org/example/Ejer10ContarVocales/Ejer10ContarVocales.java
@@ -1,7 +1,41 @@
 package org.example.Ejer10ContarVocales;
 
-public class Ejer10ContarVocales {
-    public static void main(String[] args) {
+import javax.swing.table.TableRowSorter;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class Ejer10ContarVocales {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        char[] letras={'a','e','i','o','u'};
+
+        Path ficheiro= Paths.get("ficheiro.txt");
+        if (!Files.exists(ficheiro)){
+            System.err.println("Non exise o ficheiro "+ficheiro.getFileName());
+            System.exit(2);
+        }
+
+        Path ficheiroSaida=Paths.get("ficheiroSaida.txt");
+        Files.deleteIfExists(ficheiroSaida);
+
+        ProcessBuilder builder=new ProcessBuilder();
+        for (char letra:letras){
+            builder.command("java","-cp",".\\target\\classes\\","org.example.Ejer10ContarVocales.ContarVocales","./"+ficheiro.getFileName(),String.valueOf(letra));
+            Process proceso=builder.start();
+            proceso.waitFor();
+        }
+
+        if (!Files.exists(ficheiroSaida)){
+            System.out.println("Erro, non existe o ficheiro");
+            System.exit(1);
+        }
+        try(BufferedReader output=new BufferedReader(new FileReader(ficheiroSaida.toFile()))){
+            String linea;
+            while ((linea=output.readLine())!=null){
+                System.out.println(linea);
+            }
+        }
     }
 }
