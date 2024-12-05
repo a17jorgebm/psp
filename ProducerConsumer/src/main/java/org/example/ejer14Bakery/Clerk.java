d193d9145e1971e27cde730f05b7f52b9e7b2e56
Jorge Blanco Mariño <a17jorgebm@iessanclemente.net>
2024-12-05 19:44:48 +0100

    ejer bakery pero non me convence, os clientes teñen que esperar para coller ticket se o panadeiro esta atendendo a alguen

--- ProducerConsumer/src/main/java/org/example/ejer14Bakery/Clerk.java
+++ ProducerConsumer/src/main/java/org/example/ejer14Bakery/Clerk.java
@@ -3,8 +3,7 @@ package org.example.ejer14Bakery;
 import java.util.Random;
 
 public class Clerk implements Runnable{
-    private static final int MAX_CLIENTS_TO_SERVE=100;
-    private static final Random random=new Random();
+
 
 
     private TakeANumber takeANumber;
@@ -15,14 +14,10 @@ public class Clerk implements Runnable{
 
     @Override
     public void run() {
-        for (int i=0;i<MAX_CLIENTS_TO_SERVE;i++){
-            try {
-                takeANumber.callCustomer();
-                Thread.sleep(random.nextInt(1001));
-            }catch (InterruptedException e){
+        try{
+            takeANumber.callCustomers();
+        }catch (InterruptedException e){
 
-            }
         }
-
     }
 }
--- ProducerConsumer/src/main/java/org/example/ejer14Bakery/TakeANumber.java
+++ ProducerConsumer/src/main/java/org/example/ejer14Bakery/TakeANumber.java
@@ -1,36 +1,59 @@
 package org.example.ejer14Bakery;
 
+import java.util.Random;
+
 public class TakeANumber {
     private static final int MAX_CLIENTS_TO_SERVE=100;
+    private static final Random random=new Random();
 
     int currentNumber;
     int lastNumberTaken;
+    int numberOfCustomersInLine;
+    boolean breadOfCurrentCustomerIsReadyToTake;
 
     public TakeANumber() {
-        this.currentNumber=1;
+        this.currentNumber=0;
         this.lastNumberTaken=0;
+        this.numberOfCustomersInLine=0;
+        this.breadOfCurrentCustomerIsReadyToTake=false;
     }
 
     synchronized void takeNumberAndWait(Customer customer) throws InterruptedException{
+        customer.setTicketNumber(++lastNumberTaken); //customer takes number
+        if (numberOfCustomersInLine==0){
+            System.out.println("Calling clerk");
+            notifyAll(); //if no one was waiting to be served, we notify the clerk that someone entered the bakery
+        }
+        numberOfCustomersInLine++;
 
-        customer.setTicketNumber(++lastNumberTaken);
-        notifyAll(); //avisa al panadero de que alguien entró
         System.out.println(String.format("Customer %d takes ticket %d",customer.getClientId(),customer.getTicketNumber()));
 
-        while (currentNumber!=customer.getTicketNumber()){
+        //while the Customer number is not the current number to be served, or the clerk is not ready to give the bread, the Customer waits
+        while (currentNumber!=customer.getTicketNumber() || !breadOfCurrentCustomerIsReadyToTake){
             wait();
         }
 
-        System.out.println(String.format("Customer %d with ticker %d served",customer.getClientId(),customer.getTicketNumber()));
+        //we update the number of customers waiting
+        numberOfCustomersInLine--;
+        breadOfCurrentCustomerIsReadyToTake=false;
+
+        System.out.println(String.format("Customer %d with ticket %d served",customer.getClientId(),customer.getTicketNumber()));
+
+        notifyAll(); //we notify the clerk so he attends next customer
     }
 
-    synchronized void callCustomer() throws InterruptedException{
-        while (currentNumber>lastNumberTaken){
-            System.out.println("Clerk waiting (there are no clients to serve)");
+    synchronized void callCustomers() throws InterruptedException{
+        for (int i=0;i<MAX_CLIENTS_TO_SERVE;i++){
+            if (numberOfCustomersInLine==0){ //if there are no customers in the bakery, waits for them to come
+                System.out.println("Clerk waiting (there are no clients to serve)");
+                wait();
+            }
+            currentNumber++;
+            System.out.println("Clerk serving ticket "+currentNumber);
+            Thread.sleep(random.nextInt(1001));
+            breadOfCurrentCustomerIsReadyToTake=true;
+            notifyAll();
             wait();
         }
-        System.out.println("Clerk serving ticket "+currentNumber);
-        currentNumber++;
-        notifyAll();
     }
 }
