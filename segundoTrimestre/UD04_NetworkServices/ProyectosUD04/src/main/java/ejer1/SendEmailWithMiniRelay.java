069f2de544d313456804badb4432caf2451826c7
Jorge Blanco <a17jorgebm@iessanclemente.net>
2025-02-13 14:29:57 +0100

    comezo emails

---
+++ segundoTrimestre/UD04_NetworkServices/ProyectosUD04/src/main/java/ejer1/SendEmailWithMiniRelay.java
@@ -0,0 +1,50 @@
+package ejer1;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class SendEmailWithMiniRelay {
+    private static final Path CONFIGURATION_FILE = Paths.get("properties.config");
+
+    public static void main(String[] args) {
+        Properties properties = loadProperties();
+
+        String sender = "basurerodebj@gmail.com";
+        String receiver="alt.xw-15w5gb0@yopmail.com";
+        Session session = Session.getInstance(properties);
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender,"Jorge Blanco"));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver,"Outro Jorge"));
+            message.setSubject("Hello from Java");
+            message.setText("Email sent from Java app and captured by Mailslurper.");
+            Transport.send(message);
+            System.out.println("Email sent.");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("Error sending mail.");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Properties loadProperties(){
+        Properties properties = new Properties();
+        try(FileInputStream fileInputStream = new FileInputStream(CONFIGURATION_FILE.toFile())){
+            properties.load(fileInputStream);
+            return properties;
+        }catch (IOException e){
+            System.out.println("No ha sido posible cargar el fichero de configuración: "+ e.getMessage());
+            System.exit(1);
+            return null;
+        }
+    }
+}
